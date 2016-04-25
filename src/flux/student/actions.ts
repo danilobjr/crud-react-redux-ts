47f77310b1dad15261597b774ac69c4a3798e1a5
Danilo Barros <danilobjr@gmail.com>
2016-04-25 18:57:04 -0300

    editStudent action

--- src/flux/student/actions.ts
+++ src/flux/student/actions.ts
@@ -5,6 +5,7 @@ export const CHANGE_SEARCH_TERM = 'CHANGE_SEARCH_TERM';
 export const SET_STUDENT_TO_REMOVE = 'SET_STUDENT_TO_REMOVE';
 export const ADD_STUDENT = 'ADD_STUDENT';
 export const REMOVE_STUDENT = 'REMOVE_STUDENT';
+export const EDIT_STUDENT = 'EDIT_STUDENT';
 
 export function changeSearchTerm(searchTerm: string) {
     return {
@@ -31,5 +32,12 @@ export function removeStudent(registrationNumber: string) {
     return {
         type: REMOVE_STUDENT,
         registrationNumber
+    };
+}
+
+export function editStudent(editedStudent: IStudentModel) {
+    return {
+        type: EDIT_STUDENT,
+        editedStudent
     };
 }
