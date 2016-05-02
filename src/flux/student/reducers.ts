d5d2676c3fdb44b84a960a97fee7958d358121d2
Danilo Barros <danilobjr@gmail.com>
2016-05-02 17:22:53 -0300

    removing non-used action types

--- src/flux/student/reducers.ts
+++ src/flux/student/reducers.ts
@@ -1,15 +1,15 @@
 import * as _ from 'lodash';
 import * as uuid from 'node-uuid';
 import { IStudentViewModel } from './../../models';
-import { ADD_STUDENT, REMOVE_STUDENT, STUDENTS_RECEIVED,
-         EDIT_STUDENT, CHANGE_SEARCH_TERM, SET_STUDENT_TO_REMOVE } from './actions';
+import { REMOVE_STUDENT_FROM_LIST, STUDENTS_RECEIVED,
+         CHANGE_SEARCH_TERM, SET_STUDENT_TO_REMOVE } from './actions';
 
 export function students(state = [], action): IStudentViewModel[] {
     switch (action.type) {
         case STUDENTS_RECEIVED: {
             return [].concat(action.students);
         }
-        case REMOVE_STUDENT: {
+        case REMOVE_STUDENT_FROM_LIST: {
             const studentToRemove = _.find(state, (student) => {
                 return student.id === action.id
             });
@@ -20,20 +20,6 @@ export function students(state = [], action): IStudentViewModel[] {
                 ...state.slice(0, index),
                 ...state.slice(index + 1)
             ];
-        }
-        case ADD_STUDENT:
-            return [
-                ...state,
-                action.newStudent
-            ];
-        case EDIT_STUDENT: {            
-            return state.map<IStudentViewModel>(student => {
-                if (student.registrationNumber === action.editedStudent.registrationNumber) {
-                    return _.assign({}, student, action.editedStudent);
-                }
-                
-                return student;
-            });
         }
         default:
             return state;
